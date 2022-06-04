ddfef93c99d7c8c21cf806abea08bdbffb348abd
Lukas Nehrke <lukasnehrke@protonmail.com>
2022-06-04 10:33:30 +0200

    fix: font issues (#48)
    * fix: font issues
    
    * fix: ts complaining about jsx setting

--- packages/vulpo/gatsby-browser.tsx
+++ packages/vulpo/gatsby-browser.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 
 import Layout from "./src/components/layout";
 import "./src/styles/global.css";
+import "./src/styles/lecture.css";
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
--- packages/vulpo/tailwind.config.js
+++ packages/vulpo/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   darkMode: "class",
   content: ["./plugins/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "-apple-system", "sans-serif"],
+      mono: ["Source Code Pro", "monospace"],
+    },
     extend: {
       colors: {
         theme: "var(--theme-color)",
